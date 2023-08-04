87dd94ef2eb45a87c5abb1c68819bf0bbb5b711b
Issam <ihannache@ferlab.bio>
2023-08-04 15:35:12 -0400

    feat: INFRA-454 refacotor output member group

--- output.tf
+++ output.tf
@@ -1,15 +1,3 @@
-output "groups" {
-  value = {
-    bastion = openstack_networking_secgroup_v2.k8_bastion
-    master_client = openstack_networking_secgroup_v2.k8_master_client
-    worker_client = openstack_networking_secgroup_v2.k8_worker_client
-    worker = openstack_networking_secgroup_v2.k8_worker
-    master = openstack_networking_secgroup_v2.k8_master
-    load_balancer = openstack_networking_secgroup_v2.k8_load_balancer
-    load_balancer_tunnel = openstack_networking_secgroup_v2.k8_load_balancer_tunnel
-  }
-}
-
 output "member_group" {
    value = openstack_networking_secgroup_v2.kubernetes_member
 }
