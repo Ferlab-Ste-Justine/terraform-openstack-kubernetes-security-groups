5109e476ff2d6a3e32bf47ea7adf1bcbacb527ca
Issam <ihannache@ferlab.bio>
2024-03-20 12:32:56 -0400

    feat: INFRA-403 Remove sec group for specific vm

--- security_groups.tf
+++ security_groups.tf
@@ -333,13 +333,12 @@ resource "openstack_networking_secgroup_rule_v2" "k8_worker_client_access" {
   security_group_id = openstack_networking_secgroup_v2.k8_worker.id
 }
 
-// Allow outbound traffic to Jeremy vm
 resource "openstack_networking_secgroup_rule_v2" "workers_to_spark_driver_dynamic_port" {
   direction         = "egress"
   ethertype         = "IPv4"
   protocol          = "tcp"
   port_range_min    = 1
   port_range_max    = 65535
-  remote_ip_prefix  = "198.168.188.242/32"
+  remote_group_id   = openstack_networking_secgroup_v2.automation_server_bastion.id
   security_group_id = openstack_networking_secgroup_v2.k8_worker.id
 }
